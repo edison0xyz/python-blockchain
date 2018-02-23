48c6a44ac86ee39a28fa19401e9b6a000f421e86
edison <edison.limjh@gmail.com>
2018-02-22 20:50:27 -0500

    edits to prevent double spending on transaction pool

--- Blockchain.py
+++ Blockchain.py
@@ -1,6 +1,4 @@
-# blockchain.py: simple implementation of blockchain. Does not
-# support multi-user platform. POW is too simple too. 
-
+# blockchain.py: simple implementation of blockchain
 
 from time import time
 import hashlib
@@ -24,6 +22,79 @@ class Blockchain:
         self.chain.append(blk)
         debug_print("gen block created")
 
+    # newTransaction - interface method
+    def newTransaction(self, sender, recipient, message):
+
+        if not self.validateTransaction(sender, message) and self.getLastBlock()['index'] != 1:
+            return False
+        d = {}
+        d['sender'] = sender
+        d['recipient'] = recipient
+        d['message'] = message
+        self.current_transactions.append(d)
+        return True
+
+    # mining function: mine the current transactions into a block
+    # before putting it on the blockchain. Uses POW. 
+    def mine(self, userid):
+        debug_print("mining block")
+        last_blk = self.getLastBlock()
+
+        prev_proof = last_blk['proof']
+        proof = self.proof_of_work(prev_proof)
+
+        # once proof of work is verified, add rewards and mine the block
+        self.newTransaction(0, userid, 100)
+        newBlock = self.create_block(prev_proof, proof)
+        self.chain.append(newBlock)
+        self.current_transactions = [] 
+
+        debug_print("Mined block: {}".format(newBlock['index']))
+
+    def getLastBlock(self):
+        return self.chain[-1]
+
+    # given a userid, returns the balance
+    def getBalance(self, user_address): 
+        amount = 0.0
+        for blk in self.chain[1:]:
+            transactions = blk['transactions']
+            for txn in transactions:
+                if txn['recipient'] == user_address:
+                    amount += float(txn['message'])
+                if txn['sender'] == user_address:
+                    amount -= float(txn['message'])
+        return amount
+
+    # helper - to check for double-spending before coin is mined
+    def getBalanceFromCurrentTransactions(self, user_address):
+        amount = 0.0
+        for txn in self.current_transactions:
+            if txn['recipient'] == user_address:
+                amount += float(txn['message'])
+            if txn['sender'] == user_address:
+                amount -= float(txn['message'])
+        return amount
+
+    # helper function for newTransaction. Given a sender address, validate if
+    # user has sufficient balance
+    # isMinder (boolean): if it is a miner, then we will check only against
+    # confirmed blocks. If we are checking a transaction before putting it on
+    # the chain, then we have to check against the current transactions
+
+    def validateTransaction(self, sender, message):
+        if not str(message).isdigit() or float(message) < self.token_currentsupply:
+            return False
+        amount = float(message)
+        # if sender == 0, this transaction is a mine transaction
+        # ignore validation if currentblock is the genesis block
+        if sender != 0:
+            confirmedBalance = self.getBalance(sender)
+            balanceFromTransactionPool = self.getBalanceFromCurrentTransactions(sender)
+            if confirmedBalance + balanceFromTransactionPool < amount:
+                return False
+        return True
+
     @staticmethod
     def hash(block): 
         blk_str = json.dumps(block).encode()
@@ -42,59 +113,7 @@ class Blockchain:
         return block
 
 
-    def getLastBlock(self):
-        return self.chain[-1]
-
-    def getBalance(self, userid): 
-        amount = 0.0
-        for blk in self.chain[1:]:
-            transactions = blk['transactions']
-            for txn in transactions:
-                if txn['recipient'] == userid:
-                    amount += float(txn['message'])
-                if txn['sender'] == userid:
-                    amount -= float(txn['message'])
-        return amount
-
-
-    def validateTransaction(self, sender, recipient, message):
-        if not str(message).isdigit() or float(message) < self.token_currentsupply:
-            return False
-        amount = float(message)
-        if sender != 0 and self.getBalance(sender) < amount:
-            return False
-        return True
-
-    def newTransaction(self, sender, recipient, message):
-        if not self.validateTransaction(sender, recipient, message):
-            return 
-        if not self.validateTransaction(sender, recipient, message):
-            return
-        d = {}
-        d['sender'] = sender
-        d['recipient'] = recipient
-        d['message'] = message
-        self.current_transactions.append(d)
-
-    def mine(self, userid):
-        debug_print("mining block")
-        last_blk = self.getLastBlock()
-
-        prev_proof = last_blk['proof']
-        proof = self.proof_of_work(prev_proof)
-
-        # once proof of work is verified, add rewards and mine the block
-
-        self.newTransaction(0, userid, 100)
-        newBlock = self.create_block(prev_proof, proof)
-        self.chain.append(newBlock)
-        self.current_transactions = [] 
-
-        debug_print("Mined block: {}".format(newBlock['index']))
-
     # ===== consensus layer ==========
-
-
 
     # proof_of_work will keep running until a solution has been obtained
     def proof_of_work(self, last_proof):
--- test.py
+++ test.py
@@ -7,25 +7,26 @@ bc = Blockchain()
 def getFullChain():
     return bc.chain
 
+
 # bootstrapping data - userid 1,2,3 has mined 500 tokens each
-data = [
-    [0, 1, 500],
-    [0, 2, 500],
-    [0, 3, 500],
-]
-for x in data:
-    bc.newTransaction(x[0], x[1], x[2])
+def load():
+    data = [
+        [0, 1, 500],
+        [0, 2, 500],
+        [0, 3, 500],
+    ]
+    for x in data:
+        bc.newTransaction(x[0], x[1], x[2])
+    bc.mine(1)
 
-# test case 1
-for i in range(1,4):
-    assert bc.getBalance(i) == 0
-print("Test Case 1 Passed")
-
-# test case 1
-bc.mine(3)
-for i in range(1,4):
-    amt = 600 if i == 3 else 500
-    assert bc.getBalance(i) == amt
-print("Test Case 2 Passed")
-#bc.mine(5)
-#print(getFullChain())
+def test():
+    # test case 1
+    for i in range(1,4):
+        amt = 600 if i == 1 else 500
+        print(bc.getBalance(i))
+        assert bc.getBalance(i) == amt
+    print("Test Case 2 Passed")
+    #bc.mine(5)
+    #print(getFullChain())
+load()
+test()
